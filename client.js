6d45b276499862e4c550f345aa7702aa9a7cbdaf
Nic Wilson <nic.wilson@gmail.com>
2017-03-10 16:11:45 -0600

    Initial layout and pull in data from peopleArray

--- client.js
+++ client.js
@@ -1,1 +1,13 @@
-console.log(peopleArray);
+$(document).ready(function() {
+  appendDom(peopleArray);
+});
+
+function appendDom (array) {
+  var name;
+  var shoutout;
+  for (var i = 0; i < array.length; i++) {
+    name = array[i].name;
+    shoutout = array[i].shoutout;
+    $('.container').append("<div id='"+ i +"' data-name='" + name +"' data-shoutout='" + shoutout +"' class='thumbnail'></div>");
+  }
+}
