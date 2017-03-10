a058c58adc16aaa7a30e1ac7d44d0de6e8693a3c
Nic Wilson <nic.wilson@gmail.com>
2017-03-10 16:34:42 -0600

    Add previous and next buttons to cycle between Chiyaks

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
+var index = 0;
+var totalThumb = peopleArray.length - 1;
+
 $(document).ready(function() {
   appendDom(peopleArray);
+  addEventListeners();
+  updateSelection();
 });
 
 function appendDom (array) {
@@ -8,6 +13,45 @@ function appendDom (array) {
   for (var i = 0; i < array.length; i++) {
     name = array[i].name;
     shoutout = array[i].shoutout;
-    $('.container').append("<div id='"+ i +"' data-name='" + name +"' data-shoutout='" + shoutout +"' class='thumbnail'></div>");
+    createThumb(name, shoutout, i);
   }
+}
+
+function createThumb (name, shoutout, id) {
+  $('.container').append("<div id='"+ id +"' data-name='" + name +"' data-shoutout='" + shoutout +"' class='thumbnail'></div>");
+}
+
+function addEventListeners () {
+  $('#next').on('click', nextThumb);
+  $('#prev').on('click', prevThumb);
+}
+
+function nextThumb () {
+  $('.container').children('#' + index).removeClass('highlight');
+  if (index === totalThumb) {
+    index = 0;
+  }
+  else {
+    index++;
+  }
+  updateSelection();
+}
+
+function prevThumb () {
+  $('.container').children('#' + index).removeClass('highlight');
+  if (index === 0) {
+    index = totalThumb;
+  }
+  else {
+    index--;
+  }
+  updateSelection();
+}
+
+function updateSelection() {
+  var $el = $('.container').children('#' + index);
+  console.log($el);
+  $el.addClass('highlight');
+  $('.name').text($el.data("name"));
+  $('.shoutout').text($el.data("shoutout"));
 }
