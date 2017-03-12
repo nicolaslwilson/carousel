be438e6536bb5f4f5d2ccc0304bbdcea1505b700
Nic Wilson <nicwilson@gmail.com>
2017-03-12 18:18:40 -0500

    Fix bug where index was a string, add text to page to indicate currently selected index and total indices in carousel

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
+//Index stores the current selection in the carousel, lastThumb stores the index of the last thumbnail
 var index = 0;
-var totalThumb = peopleArray.length - 1;
+var lastThumb = peopleArray.length - 1;
 
 
 var timer = {};
@@ -7,13 +8,18 @@ timer.interval = 10000;
 timer.fadeTime = 500;
 
 $(document).ready(function() {
+  //Populate the page with data from peopleArray
   appendDom(peopleArray);
+  //Add event listeners for button clicks
   addEventListeners();
+  //Update page with information from default selection
   updateSelection();
+  //Create initial timer to advance through slides every automatically
   resetTimer();
 });
 
 function appendDom (array) {
+  //For each element in the data array passed in create a thumbnail on the page with data from the array
   var name, shoutout, chiyak;
   for (var i = 0; i < array.length; i++) {
     name = array[i].name;
@@ -21,6 +27,8 @@ function appendDom (array) {
     chiyak = new Chiyak (name, shoutout, i);
     addThumb(chiyak);
   }
+  //Set the span of text on the page that indicates number of thumbnails in the carousel equal to the length of the data array
+  $('#totalThumbs').text(' / ' + (peopleArray.length));
 }
 
 function Chiyak (name, shoutout, id) {
@@ -33,9 +41,9 @@ function addThumb (chiyak) {
   chiyak.imgUrl = imageURLFromName(chiyak.name);
 
   var thumbnail = createThumb(chiyak);
-  $('.container').append(thumbnail);
+  $('.thumbnails').append(thumbnail);
 
-  var $el = $('.container').children().last();
+  var $el = $('.thumbnails').children().last();
   $el.data({"name": chiyak.name,
             "shoutout": chiyak.shoutout,
             "imgUrl": chiyak.imgUrl
@@ -66,7 +74,8 @@ function nextThumb () {
   //Store index of previous selection
   var prevIndex = index;
   //Update index to current selection
-  if (index === totalThumb) {
+  console.log(index, lastThumb);
+  if (index === lastThumb) {
     index = 0;
   }
   else {
@@ -83,7 +92,7 @@ function prevThumb () {
   var prevIndex = index;
   //Update index to current selection
   if (index === 0) {
-    index = totalThumb;
+    index = lastThumb;
   }
   else {
     index--;
@@ -95,29 +104,41 @@ function prevThumb () {
 }
 
 function selectThumb () {
-  //Store index of previous selection
-  var prevIndex = index;
-  //Update index to current selection
-  index = $(this).attr('id');
-  //Call updateSelection to update #portrait and .thumbnail
-  updateSelection(prevIndex);
+  //We only need to update the page if the clicked div is different than the current selection
+  if (index !== $(this).attr('id')) {
+    //Store index of previous selection
+    var prevIndex = index;
+    //Update index to current selection
+    index = parseInt($(this).attr('id'));
+    //Call updateSelection to update #portrait and .thumbnail
+    updateSelection(prevIndex);
+  }
   //Reset timer
   resetTimer();
 }
 
 function updateSelection(prevIndex) {
   //Remove .highlight on previous selection
-  var $prev = $('.container').children('#' + prevIndex);
+  var $prev = $('.thumbnails').children('#' + prevIndex);
   $prev.removeClass('highlight');
   //Add .highlight to current selection
-  var $el = $('.container').children('#' + index);
+  var $el = $('.thumbnails').children('#' + index);
   $el.addClass('highlight');
+  //Change text on page to indicate current selected index
+  $('#index').text(index + 1);
   //fadeOut previous #portrait data and then call updatePortrait
   $('#portrait').fadeOut(timer.fadeTime, function (){
     updatePortrait($el);
   });
 }
 
+
+/**
+  Updates #portrait div with data from the selected
+  *@function resetTimer
+  *@return
+  **
+*/
 function updatePortrait ($el) {
   //Update #portrait div with name, shoutout and image for currently selected chiyak
   $('#portrait .name').text($el.data("name"));
@@ -127,6 +148,12 @@ function updatePortrait ($el) {
   $('#portrait').fadeIn(timer.fadeTime);
 }
 
+/**
+  Resets current timer, if any, and creates new timer
+  *@function resetTimer
+  *@return
+  **
+*/
 function resetTimer() {
   //If a timer already exists, clear it
   if (timer.id) {
